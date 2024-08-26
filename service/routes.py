87a44b37212072e75d513d248c8fbc90e439d82a
fecabra <cabral.felipe@gmail.com>
2024-08-26 15:52:55 -0400

    added correction

--- service/routes.py
+++ service/routes.py
@@ -61,7 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 @app.route("/accounts", methods=["GET"])
-    def list_accounts():
+def list_accounts():
         """
         List all Accounts
         This endpoint will list all Accounts
@@ -89,7 +89,7 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
-    def update_accounts(account_id):
+def update_accounts(account_id):
         """
         Update an Account
         This endpoint will update an Account based on the posted data
@@ -107,7 +107,7 @@ def get_accounts(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-    def delete_accounts(account_id):
+def delete_accounts(account_id):
         """
         Delete an Account
         This endpoint will delete an Account based on the account_id that is requested
